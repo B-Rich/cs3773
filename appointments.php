98ae71dd44f2827ce1430fa971348210175a1576
rmstrautman <rmstrautman@gmail.com>
2013-04-14 00:30:25 -0500

    added appointments.php

---
+++ appointments.php
@@ -0,0 +1,37 @@
+<html>
+<?php
+   session_start();
+   require_once('db.php');
+   $conn = connect_db();
+
+   //get current date for query
+   $date_format = "Y-m-d";
+   $curr_date = date($date_format);
+
+   //get time, last name, first name from database for all appointments
+   //for the current day, list in order of time (ascending) 
+   $query = "SELECT time, fname, lname
+             FROM User, Appointment
+             WHERE username=patient AND date='$curr_date'
+             ORDER BY time";
+   
+   //execute query and get result
+   $result = mysqli_query($conn, $query);
+   $n = $result->num_rows;
+  
+   //print table with time in first colum, lastname and first name in second column
+   echo "<table>";
+   for ($i = 1; $i <= $n; $i++){
+      $row = mysqli_fetch_assoc($result);
+      $time = strtotime($row['time']);
+      $time = date("g:i A", $time);
+      echo "<tr><td>" .$time. "</td><td>".$row['lname'].", ". 
+            $row['fname']."</td></tr>"; 
+   }
+   echo "</table>";
+
+   //close connection
+   mysqli_close($conn);
+   
+?>
+</html>
