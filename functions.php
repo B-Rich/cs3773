7b524d2a535b635708ace4e14b7d02ec423143fb
amantrip <amantrip@cs.utsa.edu>
2013-04-15 17:58:02 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,25 +1,4 @@
 <?php 
-$dbhost  = 'localhost';    
-$dbname  = 'wiava';      
-$dbuser  = 'akhi';  
-$dbpass  = 'leadershiphonors';   
-$appname = "Wiava.com"; 
-
-mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
-mysql_select_db($dbname) or die(mysql_error());
-
-function createTable($name, $query)
-{
-    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
-    echo "Table '$name' created or already exists.<br />";
-}
-
-function queryMysql($query)
-{
-    $result = mysql_query($query) or die(mysql_error());
-   return $result;
-}
-
 function destroySession()
 {
     $_SESSION=array();
@@ -38,17 +17,5 @@ function sanitizeString($var)
     return mysql_real_escape_string($var);
 }
 
-function showProfile($user)
-{
-    if (file_exists("$user.jpg"))
-        echo "<img src='$user.jpg' align='left' />";
 
-    $result = queryMysql("SELECT * FROM profiles WHERE user='$user'");
-
-    if (mysql_num_rows($result))
-    {
-        $row = mysql_fetch_row($result);
-        echo stripslashes($row[1]) . "<br clear=left /><br />";
-    }
-}
 ?>
