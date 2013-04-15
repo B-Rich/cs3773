0aa57c99e6be6bf95398ad54aa541225642c7e39
rmstrautman <rmstrautman@gmail.com>
2013-04-15 00:33:04 -0500

    Added searchchart.php

---
+++ searchchart.php
@@ -0,0 +1,50 @@
+<html>
+<body>
+
+<?php
+   session_start();
+   include_once('db.php');
+   // session not logged in so redirect to login page
+   if(!isset($_SESSION['user'])){
+      header("Location: login.php");
+   } 
+   // user does not have permission to view page
+   if(!($_SESSION['type'] == 'doctor' || $_SESSION['type'] == 'nurse')){
+      echo "Permission denied!";
+      die;
+   } 
+   //connect to database
+   $conn = connect_db();
+?>
+
+   <form action="searchchart.php" method="POST">
+   Search Patient: <input type = "text" name = "searchString" size = "20">
+   <input type = "submit" value = "Search" name = "search">
+</form>
+<br>
+
+<?php
+   //search patients based on substring match in first or last name
+   if (isset($_POST['search'])){
+      //$searchString = sanitizeString($_POST['searchString']);
+      $searchString = $_POST['searchString'];
+      $query = "select fname, minit, lname, dob, username
+                from User 
+                where (fname like '%$searchString%'
+                or lname like '%$searchString%')
+                and type='patient'";
+      $result = mysqli_query($conn, $query);
+
+      //display results - plain text for now, will need to link to chart
+      if (mysqli_num_rows($result) == 0){
+         echo "No patients found.";
+      }
+      else{
+         while ($row = mysqli_fetch_array($result, MYSQL_NUM)){
+             echo "$row[0] $row[1] $row[2], DOB: $row[3]";
+         }
+      }
+   }
+?>
+</body>
+</html>
