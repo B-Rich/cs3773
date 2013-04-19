184091ae0bfd09702a7821035e871d734bd7265b
mrjavoman <mrjavoman@hotmail.com>
2013-04-19 13:12:28 -0500

    added a change to db.php

--- db.php
+++ db.php
@@ -15,6 +15,29 @@ function connect_db(){
    return $conn;
 }
 
+
+// function checks if user exists
+function userExists( $userName ){
+
+   $exists = false;
+   // open connection to the DB
+   $conn = connect_db();
+   $query = "SELECT username FROM User WHERE username = '$userName'";
+   
+   // find if $username is in the database
+   $result = mysqli_query($conn, $query);
+   if (mysqli_num_rows($result) == 1)
+   {
+      $exists = 1;
+   }
+       
+   // execute the query and return the results
+   mysqli_query($conn, $query);  
+   //close connection to the database
+   mysqli_close($conn);  
+   
+   return $exists;     
+}
 // function takes a connection and a string that represents a sql command
 // this function is for general use, returns the executed sql command
 function execSql($conn, $sql){
